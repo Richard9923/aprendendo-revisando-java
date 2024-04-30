b3f8a7092e9b04732b1e44c0866e3732c1ed8450
Richard <programmingislife23@gmail.com>
2024-04-30 19:30:49 -0300

    aprendendo atalho para escrever o print

--- newProgram.java
+++ newProgram.java
@@ -5,6 +5,10 @@ public class newProgram {
 	public static void main(String[] args) {
 		System.out.println("I love pizzaa.");
 		System.out.print("It's really good.");	
+        System.out.println();
+        System.out.println();
+
+        // Atalho para escrever print (sysout. ctrl + space.)
 		
 		// Learning to comment. 
 		// Aprendendo a comentar.
