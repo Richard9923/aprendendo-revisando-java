e0513960409400da0e0e5460df08cf8a36358a68
Richard <programmingislife23@gmail.com>
2024-04-30 19:57:58 -0300

    displaying my name.

--- learningVariables.java
+++ learningVariables.java
@@ -5,14 +5,21 @@ public class learningVariables {
         int x = 123; // Initialization int number.
         float y = 3.14f; // Initialization number with decimal. FLOAT.
         double u = 3.14; // Initialization number with decibal. Is better cause I don't need to tap the 'f' like before.
+        boolean z = false; // boolean can just hold true or false
+        char symbol = '@';
+        String name = "Richard";
 
         System.out.println("Variable with a INT number.");
         System.out.println("My int number is: "+x);
 
         System.out.println("Variable with a number with a decimal part.");
         System.out.println("My number with decimals is: "+y);
+
         System.out.println("Number with decimal parts. Using 'double'.");
         System.out.println("My number with decimal part using double is: "+u);
+
+        System.out.println("Display my name.");
+        System.out.println("Hello "+name);
 
     }
 }
