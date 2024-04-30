da1fb66ca78797a5fb4a2eec98cb26f6b11c0f5c
Richard <programmingislife23@gmail.com>
2024-04-30 20:31:49 -0300

    creating inputs of name and age.

--- learningUserInput.java
+++ learningUserInput.java
@@ -8,8 +8,11 @@ public class learningUserInput {
 
         System.out.println("What is your name? ");
         String name = scanner.nextLine();
+        System.out.println("How old are you? ");
+        int age = scanner.nextInt();
 
         System.out.println("Hello "+name);
+        System.out.println("You are "+age+ " years old");
 
 
     }
