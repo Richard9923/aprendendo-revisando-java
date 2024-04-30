5b111652957bab24eb1b33a33a27823dfd6ce2a1
Richard <programmingislife23@gmail.com>
2024-04-30 19:52:56 -0300

    learning to use double instead of float

--- learningVariables.java
+++ learningVariables.java
@@ -4,12 +4,15 @@ public class learningVariables {
 
         int x = 123; // Initialization int number.
         float y = 3.14f; // Initialization number with decimal. FLOAT.
+        double u = 3.14; // Initialization number with decibal. Is better cause I don't need to tap the 'f' like before.
 
         System.out.println("Variable with a INT number.");
         System.out.println("My int number is: "+x);
 
         System.out.println("Variable with a number with a decimal part.");
         System.out.println("My number with decimals is: "+y);
+        System.out.println("Number with decimal parts. Using 'double'.");
+        System.out.println("My number with decimal part using double is: "+u);
 
     }
 }
