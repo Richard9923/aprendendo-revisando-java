0c91e8baca621cf1032d83e3177a8c749a062775
Richard <programmingislife23@gmail.com>
2024-04-30 20:08:37 -0300

    mescling variables

--- mergindTwoVariables.java
+++ mergindTwoVariables.java
@@ -1,3 +1,20 @@
 public class mergindTwoVariables {
+
+    public static void main(String[] args) {
+        String x = "water";
+        String y = "Kool-Aid";
+        String temp;
+
+        temp = x;
+        x=y;
+        y=temp;
+
+        System.out.println("x: "+x);
+        System.out.println("y: "+y);
+        System.out.println();
+        
+    }
+
+
     
 }
