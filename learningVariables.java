55d508c2b2c8ecc082d069ec2e92c4c5f6c02660
Richard <programmingislife23@gmail.com>
2024-04-30 19:50:07 -0300

    aprendendo a criar variavéis int e float em java

--- learningVariables.java
+++ learningVariables.java
@@ -1,5 +1,15 @@
 public class learningVariables {
     public static void main(String[] args) {
         System.out.println("Aprendendo a escrever variáveis em Java.");
+
+        int x = 123; // Initialization int number.
+        float y = 3.14f; // Initialization number with decimal. FLOAT.
+
+        System.out.println("Variable with a INT number.");
+        System.out.println("My int number is: "+x);
+
+        System.out.println("Variable with a number with a decimal part.");
+        System.out.println("My number with decimals is: "+y);
+
     }
 }
