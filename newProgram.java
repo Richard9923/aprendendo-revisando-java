fb286cf8ea2d14fd8a47d175c3e42c9bccf9e480
Richard <programmingislife23@gmail.com>
2024-04-30 19:24:15 -0300

    aprendendo print em java

---
+++ newProgram.java
@@ -0,0 +1,10 @@
+
+
+public class newProgram {
+
+    public static void main(String[] args) {
+        System.out.println("I love pizzaa");
+        System.out.print("It's really good");
+    }
+
+ }
