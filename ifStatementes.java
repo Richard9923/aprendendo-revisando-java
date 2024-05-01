0a6c1426db1dea0baf4acc7f93c6067ab0034025
Richard <programmingislife23@gmail.com>
2024-05-01 11:18:24 -0300

    aprendendo else if statementes

---
+++ ifStatementes.java
@@ -0,0 +1,21 @@
+public class ifStatementes {
+    
+    public static void main(String[] args) {
+        // if statements in java
+
+        int age = 18;
+
+        if(age >= 18) {
+            System.out.println("You are an adult!");
+
+        }
+        else if(age>=75) {
+            System.out.println("Ok Boomer!");
+
+        }
+        else{
+            System.out.println("You're not an adult!");
+        }
+            
+    }
+}
