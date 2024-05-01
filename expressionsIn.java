ec9be068d0feb20c7488283bd7408cdbad506c94
Richard <programmingislife23@gmail.com>
2024-04-30 21:08:08 -0300

    learning expressions in java

---
+++ expressionsIn.java
@@ -0,0 +1,23 @@
+public class expressionsIn {
+
+    public static void main(String[] args) {
+
+        // expression = operands and operators
+        // operands = values, variables, numbers, quantity
+        // operatorts = + - * / % 
+
+        int friends = 10;
+        
+        friends++;
+        System.out.println(friends);
+
+        double moreFriends = 10;
+        moreFriends /= 3;
+        System.out.println(moreFriends);
+
+
+
+
+    }
+    
+}
