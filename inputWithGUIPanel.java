030befe6537bd849f8ee6a806ce8773f687bcac4
Richard <programmingislife23@gmail.com>
2024-04-30 21:14:30 -0300

    learning GUI panel in Java

---
+++ inputWithGUIPanel.java
@@ -0,0 +1,14 @@
+
+import javax.swing.JOptionPane;
+
+public class inputWithGUIPanel {
+
+    public static void main(String[] args) {
+
+        String name = JOptionPane.showInputDialog("Enter your name.");
+
+
+
+    }
+    
+}
