1a40f2c3291dee2b2215c11969a0dd11f2fd732c
Richard <programmingislife23@gmail.com>
2024-04-30 21:19:15 -0300

    Criando primeiros paines de input

--- inputWithGUIPanel.java
+++ inputWithGUIPanel.java
@@ -6,6 +6,8 @@ public class inputWithGUIPanel {
     public static void main(String[] args) {
 
         String name = JOptionPane.showInputDialog("Enter your name.");
+        JOptionPane.showMessageDialog(null, "Hello "+name);
+
 
 
 
