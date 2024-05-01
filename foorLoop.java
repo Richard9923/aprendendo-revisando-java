38405d1f2575446864f0e094226e1b10d08453c6
Richard <programmingislife23@gmail.com>
2024-05-01 12:16:39 -0300

    for loop

---
+++ foorLoop.java
@@ -0,0 +1,10 @@
+public class foorLoop {
+    // for loop
+
+    public static void main(String[] args) {
+        for(int i = 10; i>= 0; i--) {
+            System.out.println(i);
+        }
+        System.out.println("Happy new year!");
+    }
+}
