a5a1d6f8db58dab3d0029b707df153f4ff4087ee
Richard <programmingislife23@gmail.com>
2024-05-02 18:37:56 -0300

    arrayList.java

---
+++ arrayList.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+
+public class arrayList {
+    public static void main(String[] args) {
+        ArrayList<String> food = new ArrayList<String>();
+
+        food.add("pizza");
+        food.add("hamburger");
+        food.add("hotdog");
+
+        food.set(0, "sushi");
+        food.remove(2);
+        food.clear();
+
+        for(int i=0; i<food.size(); i++) {
+            System.out.println(food.get(i));
+        }
+    }
+}
