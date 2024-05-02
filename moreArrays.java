fa15ec17817a4c1507b5696186d72fb32c7995c0
Richard <programmingislife23@gmail.com>
2024-05-02 18:12:26 -0300

    2d arrays rows and columns

--- moreArrays.java
+++ moreArrays.java
@@ -1,5 +1,24 @@
 public class moreArrays {
     public static void main(String[] args) {
-        
+        String[][] cars = new String [3][3];
+
+        cars[0][0] = "Camaro";
+        cars[0][1] = "Corvette";
+        cars[0][2] = "Silverado";
+        cars[1][0] = "Mustang";
+        cars[1][1] = "Ranger";
+        cars[1][2] = "F-150";
+        cars[2][0] = "Ferrari";
+        cars[2][1] = "Lambo";
+        cars[2][2] = "Tesla";
+
+        for (int i=0; i < cars.length; i++) {
+            System.out.println();
+            for (int j = 0; j<cars[i].length; j++) {
+                System.out.println(cars[i][j]+ " ");
+
+            }
+
+        }
     }
 }
