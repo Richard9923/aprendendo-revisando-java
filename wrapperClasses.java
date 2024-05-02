ba0f43194d69a7909d0ed340ab1edf45a25afe03
Richard <programmingislife23@gmail.com>
2024-05-02 18:32:59 -0300

    wrapper classes

---
+++ wrapperClasses.java
@@ -0,0 +1,10 @@
+public class wrapperClasses {
+    public static void main(String[] args) {
+        
+        Boolean a = true;
+        Character b = '@';
+        Integer c = 123;
+        Double d = 3.14;
+        String e = "Bro";
+    }
+}
