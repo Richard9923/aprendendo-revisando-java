387889a15ce501486e25396b8b6ce88760eb1bc4
Richard <programmingislife23@gmail.com>
2024-05-02 18:46:11 -0300

    reescrevendo arraylist

--- arrayList.java
+++ arrayList.java
@@ -2,18 +2,22 @@ import java.util.ArrayList;
 
 public class arrayList {
     public static void main(String[] args) {
-        ArrayList<String> food = new ArrayList<String>();
+        ArrayList <String> food = new ArrayList<String>();
 
+        System.out.println(food);
         food.add("pizza");
-        food.add("hamburger");
         food.add("hotdog");
+        food.add("pastel");
 
-        food.set(0, "sushi");
+        food.set(1, "sushi");
         food.remove(2);
-        food.clear();
 
-        for(int i=0; i<food.size(); i++) {
+
+
+        for (int i=0; i<food.size(); i++) {
             System.out.println(food.get(i));
+
         }
+
     }
 }
