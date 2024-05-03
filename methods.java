95e9688b4fab7b51aebc15f182d33ef745eecef4
Richard <programmingislife23@gmail.com>
2024-05-03 18:33:30 -0300

    aprendendo sobre methods em java

---
+++ methods.java
@@ -0,0 +1,17 @@
+public class methods {
+
+    public static void main(String[] args) {
+        
+        int age = 21;
+        String name = "Bro";
+        hello(name, age);
+
+
+    }
+
+    static void hello(String name, int age) {
+        System.out.println(name);
+        System.out.println("You are "+age);
+    }
+    
+}
