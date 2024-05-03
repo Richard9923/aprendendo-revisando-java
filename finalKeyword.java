f980325b0aba5e8b9376b8d87dedbd40f5e6102d
Richard <programmingislife23@gmail.com>
2024-05-03 20:01:00 -0300

    final keyword java

---
+++ finalKeyword.java
@@ -0,0 +1,13 @@
+public class finalKeyword {
+    
+
+    public static void main(String[] args) {
+       
+       final double pi = 3.14159;
+
+        pi = 4;
+
+        System.out.println(pi);
+    }
+
+}
