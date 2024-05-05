41eefd6e6bdaa2e69ef9a421b2c4df3029d9dec7
Richard <programmingislife23@gmail.com>
2024-05-04 22:48:22 -0300

    praticando criando objetos

--- contructor.java
+++ contructor.java
@@ -1,5 +1,16 @@
 public class contructor {
     
-    
+    public static void main(String[] args) {
+
+   
+
+
+        human human = new human("Rick", 65, 70);
+        human human2 = new human("Morty", 16, 50);
+
+        System.out.println(human2.name);
+
+    }
+
 
 }
--- human.java
+++ human.java
@@ -1,3 +1,17 @@
 public class human {
     
+    String name;
+    int age;
+    double weight;
+
+    human(String name,int age,double weight){
+
+        this.name = name;
+        this.age = age;
+        this.weight  = weight;
+
+    }
+
+
+
 }
