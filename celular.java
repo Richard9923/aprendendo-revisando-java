9dc72d81f9a9fa9c4e171894e039b26613083cc7
Richard <programmingislife23@gmail.com>
2024-05-04 22:34:58 -0300

    criando objeto celular

---
+++ celular.java
@@ -0,0 +1,20 @@
+public class celular {
+
+    String marca = "Xiaomi";
+    String nome = "poco x5";
+    int preco = 800;
+
+    void ligar() {
+        System.out.println("Você ligou o celular");
+
+    }
+
+    void desligar(){
+        System.out.println("Você desligou o celular");
+    }
+
+    
+
+
+
+}
