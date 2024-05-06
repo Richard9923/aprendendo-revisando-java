ceb816dd3050a9391ea8ee74f1df112dce3bbe12
Richard <programmingislife23@gmail.com>
2024-05-06 20:06:17 -0300

    entendo e criando variavel global

--- DiceRoller.java
+++ DiceRoller.java
@@ -3,13 +3,17 @@ import java.util.Random;
 
 public class DiceRoller {
     
+    Random random;
+    int number;
 
     DiceRoller(){
-        Random random = new Random();
+        random = new Random();
+        roll();
     }
 
     void roll() {
-
+        number = random.nextInt(6)+1;
+        System.out.println(number);
     }
 
 
