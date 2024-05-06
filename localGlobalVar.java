cdaccbfbd3fd851c337d285e08d28d2b6aa94add
Richard <programmingislife23@gmail.com>
2024-05-06 19:57:29 -0300

    local global variable

---
+++ localGlobalVar.java
@@ -0,0 +1,15 @@
+public class localGlobalVar {
+   
+    public static void main(String[] args) {
+            
+        //local = declared insided a method visible only to that method
+
+        //global = declared outside a method, but within a class \
+        //visible to all parts of a class
+
+        
+
+    }
+
+
+}
