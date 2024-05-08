3c648dbad7bec22378c25ec2e60bfdcd02700df4
Richard <programmingislife23@gmail.com>
2024-05-08 19:50:53 -0300

    overloadContructors

--- overloadContructors.java
+++ overloadContructors.java
@@ -3,11 +3,14 @@ public class overloadContructors {
 
     public static void main(String[] args) {
         
+        pizza pizza = new pizza("thicc crust", "tomato", "peperonne");
+
+        System.out.println("Here are the ingredients of your pizza: ");
+        System.out.println(pizza.bread);
+        System.out.println(pizza.sauce);
+        System.out.println(pizza.cheese);
+        System.out.println(pizza.topping);
         
-
-
-
-
     }
 
 
--- pizza.java
+++ pizza.java
@@ -1,5 +1,38 @@
 public class pizza {
-    
+
+    String bread;
+    String sauce;
+    String cheese;
+    String topping;
+
+    pizza(String bread,String sauce,String cheese){
+
+        this.bread = bread;
+        this.sauce = sauce;
+        this.cheese = cheese;
+    }
 
     
+    pizza(String bread,String sauce){
+
+        this.bread = bread;
+        this.sauce = sauce;
+    }
+
+    pizza(String bread){
+
+        this.bread = bread;
+    }
+
+    pizza(String bread,String sauce,String cheese,String topping){
+
+        this.bread = bread;
+        this.sauce = sauce;
+        this.cheese = cheese;
+        this.topping = topping;
+    }
+
+    
+
+
 }
