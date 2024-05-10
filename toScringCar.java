56648586f001c043e27a1216cb3163a3eaaab019
Richard <programmingislife23@gmail.com>
2024-05-10 17:06:25 -0300

    aprendendo to string methodo

---
+++ toScringCar.java
@@ -0,0 +1,12 @@
+public class toScringCar {
+    
+    String make = "Ford";
+    String model = "Mustang";
+    String color = "red";
+    int year = 2021;
+
+    public String toString() {
+        return make +"\n "+model+" \n "+color+" \n"+year;
+    }
+
+}
---
+++ toString.java
@@ -0,0 +1,17 @@
+public class toString {
+    
+
+    public static void main(String[] args) {
+        
+        toScringCar car = new toScringCar();
+        System.out.println(car);
+
+
+
+
+    }
+
+    
+
+
+}
