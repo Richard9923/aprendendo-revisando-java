4ca6822a7806d499a0f808be2637412374871a13
Richard <programmingislife23@gmail.com>
2024-05-11 16:11:24 -0300

    aprendendo array em objetos java

---
+++ arrayObjects.java
@@ -0,0 +1,24 @@
+public class arrayObjects {
+    
+
+    public static void main(String[] args) {
+        
+
+        //Food[] refrigerator = new Food[3];
+
+        Food food1 = new Food("pizza");
+        Food food2 = new Food("hamburger");
+        Food food3 = new Food("hot dog");
+
+        Food[] refrigerator = {food1, food2, food3};
+
+        //refrigerator[0] = food1;
+        //refrigerator[1] = food2;
+        //refrigerator[3] = food3;
+
+        System.out.println(refrigerator[0].name);
+        System.out.println(refrigerator[1].name);
+        System.out.println(refrigerator[2].name);
+
+    }
+}
