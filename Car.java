965bee82b88d89996cf2189dd200d69580aafcf7
Richard <programmingislife23@gmail.com>
2024-05-12 11:51:11 -0300

    apagando todo código para recriar por motivos de pratica

--- Car.java
+++ Car.java
@@ -1,10 +1,4 @@
 public class Car {
     
-    String name;
-    
-    Car(String name){
-        this.name = name;
-
-    }
 
 }
--- Garage.java
+++ Garage.java
@@ -1,9 +1,5 @@
 public class Garage {
     
 
-    void park(Car car) {
-        System.out.println("The car "+car.name+" is parked in the garage");
-    }
-
 
 }
