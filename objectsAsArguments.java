9e10a49491342cbd05f4f10cbd4bbcb76e76cf55
Richard <programmingislife23@gmail.com>
2024-05-12 11:49:11 -0300

    Declarando mais um carro

--- objectsAsArguments.java
+++ objectsAsArguments.java
@@ -4,9 +4,11 @@ public class objectsAsArguments {
         
         Garage garage = new Garage();
 
-        Car car = new Car("BMW");
+        Car car1 = new Car("BMW");
+        Car car2 = new Car("Tesla");
 
-        garage.park(car);
+        garage.park(car1);
+        garage.park(car2);
 
 
     }
