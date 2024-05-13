38544ce0a4a467c95df6755103d54deed2f13fba
Richard <programmingislife23@gmail.com>
2024-05-13 20:01:00 -0300

    audioClip finalizado

--- audioPlayer/Main.java
+++ audioPlayer/Main.java
@@ -9,14 +9,37 @@ public class Main {
     public static void main(String[] args) throws UnsupportedAudioFileException, IOException, LineUnavailableException {
         
         Scanner scanner = new Scanner(System.in);
-        //File file = new File("audioPlayer/level_up.wav");
+        File file = new File("audioPlayer/level_up.wav");
         AudioInputStream audioStream = AudioSystem.getAudioInputStream(file);
         Clip clip = AudioSystem.getClip();
         clip.open(audioStream);
 
-        clip.start();
+        
+        String response = "";
 
-        String response = scanner.next();
+        while(!response.equals("Q")) {
+            System.out.println("P = play, Stop, R = Reset, Q = Quit");
+            System.out.println("Enter your choice: ");
+          
+            response = scanner.next();
+            response = response.toUpperCase();
+
+            switch (response) {
+                case ("P"): clip.start();  
+                    break; 
+                case ("S"): clip.stop();
+                    break;
+                case ("R"): clip.setMicrosecondPosition(0);
+                    break;
+                case ("Q"): clip.close();
+                    break;
+                default: System.out.println("Not a valid response");
+                    break;
+            }
+
+        }
+        System.out.println("Byeee!");
+
 
     }
 }
