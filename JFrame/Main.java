3456d6ce1b6a2f8a2f01543347c0e9419e96768c
Richard <programmingislife23@gmail.com>
2024-05-13 20:42:31 -0300

    finalizado

--- JFrame/Main.java
+++ JFrame/Main.java
@@ -1,7 +1,21 @@
 package JFrame;
+import java.awt.Color;
+
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
 
 public class Main {
     public static void main(String[] args) {
         
+        JFrame frame = new JFrame();
+        frame.setTitle("JFrame title goes here");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setResizable(false);
+        frame.setSize(420,420);
+        frame.setVisible(true);
+
+        ImageIcon image = new ImageIcon("logo.png");
+        frame.setIconImage(image.getImage());
+        frame.getContentPane().setBackground(Color.black);
     }
 }
