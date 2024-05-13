3fd5425db3a5ec34ca1b84f0eb02ffadc335c42a
Richard <programmingislife23@gmail.com>
2024-05-13 16:44:59 -0300

    encapisulation

---
+++ pratica/encapsulation/Car.java
@@ -0,0 +1,18 @@
+package pratica.encapsulation;
+
+public class Car {
+    
+    private String make;
+    private String model;
+    private int year;
+
+    Car(String make,String model, int year){
+        this.make = make;
+        this.model = model;
+        this.year = year;
+    }
+
+
+
+
+}
---
+++ pratica/encapsulation/Main.java
@@ -0,0 +1,12 @@
+package pratica.encapsulation;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        
+
+        Car car = new Car("Chevrolet", "Camaro", 2021);
+
+        System.out.println(car.make);
+    }
+}
