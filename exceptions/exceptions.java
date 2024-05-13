8390a5ea6e4e7960bfc0f238019494fec2c85e52
Richard <programmingislife23@gmail.com>
2024-05-13 18:28:48 -0300

    finalizado

--- exceptions/exceptions.java
+++ exceptions/exceptions.java
@@ -1,5 +1,36 @@
 package exceptions;
-
+import java.util.Scanner;
 public class exceptions {
     
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        try{
+        
+        
+        System.out.println("Enter a whole number to divide: ");
+        int x = scanner.nextInt();
+
+        System.out.println("Enter a whole number to divide: ");
+        int y = scanner.nextInt();
+
+        int z = x/y;
+        System.out.println("Result: "+z);
+        
+    }
+    catch(ArithmeticException e) {
+
+        System.out.println("You can't divide by zero!");
+        }
+    catch(java.util.InputMismatchException e) {
+        System.out.println("PLEASE ENTER A NUMBER!!");
+    }
+    catch(Exception e) {
+        System.out.println("Something went wrong");
+
+    }
+    finally {
+        scanner.close();
+    }
+
+    }
 }
